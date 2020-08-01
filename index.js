b277de63c043fbb31121c2bcba398292c11a994f
Knar Gocer <knargr@gmail.com>
2020-08-01 08:44:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  driver.key = value;
+  Object.assign(driver,{key:value});
   return driver;
 }
 
